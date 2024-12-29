78d9bbb26013846487e8155a3d4839d155ee321e
malwina-data <79791059+malwina-data@users.noreply.github.com>
2024-12-29 20:33:57 +0100

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,114 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  tags = {
+    Name = "Simple-VPC"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+    tags = {
+        Name = "MainInternetGateway"
+    }
+}
+
+
+resource "aws_subnet" "main"{
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "sa-east-1a"
+    tags = {
+      Name = "subnet_1a"
+    }
+}
+
+resource "aws_route_table" "main"{
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+  tags = {
+    Name = "security_group"
+  }
+}
+resource "aws_instance" "main" {
+  ami           = "ami-03c4a8310002221c7"  # Podaj odpowiednie AMI dla swojego regionu
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.main.id
+  associate_public_ip_address = true
+  security_groups = [aws_security_group.main.id]
+  key_name      = "key-pair"
+  tags = {
+    Name = "Instance"
+  }
+}
+resource "null_resource" "provision" {
+  provisioner "file" {
+    source      = "/Users/malwinka/Downloads/vsts-agent-linux-x64-3.248.0.tar.gz" 
+    destination =  "/home/ec2-user/vsts-agent-linux-x64-3.248.0.tar.gz"
+
+
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      private_key = file("~/.ssh/key-pair.pem")
+      host        = aws_instance.main.public_ip
+  }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+    "echo 'Checking SSH connection' && sleep 10"
+    ]
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      private_key = file("~/.ssh/key-pair.pem")
+      host        = aws_instance.main.public_ip
+  }
+  } 
+
+}
+output "subnet_id" {
+  value = aws_subnet.main.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+output "security_group_id" {
+  value = aws_security_group.main.id
+}
+output "instance" {
+  value = aws_instance.main.id
+}
+output "instance_public_ip" {
+  value = aws_instance.main.public_ip
+  description = "Public IP of the EC2 instance"
+}
