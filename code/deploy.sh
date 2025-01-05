bf5c2dd01aafa0a31c9ac50e67956e5ea3b2c7d6
Malwinka <malwina.fila@gmail.com>
2025-01-05 12:15:18 +0100

    add

--- code/deploy.sh
+++ code/deploy.sh
@@ -10,9 +10,6 @@ python3 -m venv /home/ubuntu/app/venv
 # 3. Aktywuj środowisko wirtualne
 source /home/ubuntu/app/venv/bin/activate
 
-# 4. Zainstaluj zależności w środowisku wirtualnym
-pip install -r /home/ubuntu/app/requirements.txt
-
 # 5. Uruchom aplikację w tle przy użyciu nohup (bez logów)
 nohup python3 /home/ubuntu/app/main.py &
 
