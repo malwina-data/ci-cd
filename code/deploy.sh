df107359c90b61c5a4e636c45b74ed32e7bd4619
Malwinka <malwina.fila@gmail.com>
2025-01-05 14:43:23 +0100

    m

--- code/deploy.sh
+++ code/deploy.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-
-# 1. Zainstaluj virtualenv, jeśli nie jest zainstalowany
-sudo apt-get update
-sudo apt-get install python3-venv
-
-# 2. Utwórz środowisko wirtualne w folderze aplikacji
-python3 -m venv /home/ubuntu/app/venv
-
-# 3. Aktywuj środowisko wirtualne
-source /home/ubuntu/app/venv/bin/activate
-
-# 4. Zainstaluj gunicorn
-pip install flask
-pip install gunicorn
+source /home/ubuntu/app/myenv/bin/activate
 
 nohup gunicorn -w 1 -b 0.0.0.0:5000 main:app > gunicorn.log 2>&1 &
