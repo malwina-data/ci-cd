b2827e31fe4fc50fc2cdb123030b63646b14f75c
Malwinka <malwina.fila@gmail.com>
2025-01-05 10:59:37 +0100

    add

--- code/deploy.sh
+++ code/deploy.sh
@@ -1,4 +1,17 @@
-cd /home/ubuntu/app
-pip install flask
+#!/bin/bash
 
-python3 main.py
+# 1. Zainstaluj virtualenv, jeśli nie jest zainstalowany
+sudo apt-get update
+sudo apt-get install python3-venv
+
+# 2. Utwórz środowisko wirtualne w folderze aplikacji
+python3 -m venv /home/ubuntu/app/venv
+
+# 3. Aktywuj środowisko wirtualne
+source /home/ubuntu/app/venv/bin/activate
+
+# 4. Zainstaluj zależności w środowisku wirtualnym
+pip install -r /home/ubuntu/app/requirements.txt
+
+# 5. Uruchom aplikację
+python3 /home/ubuntu/app/main.py
