a7c20cde138992a353fe42ed7776396ceb5f8f53
Malwinka <malwina.fila@gmail.com>
2025-02-02 18:49:22 +0100

    w

--- code/dostep_secrets.py
+++ code/dostep_secrets.py
@@ -9,14 +9,6 @@ def get_db_credentials():
     for secret in response.get('SecretList', []):
         secrets_list.append(secret.get('Name'))
 
-    # secrets with login and password
-    response = client.get_secret_value(SecretId=secrets_list[0])
-    secret = response['SecretString']
-    secret_dict = json.loads(secret)
-    username = secret_dict.get('username')
-    password = secret_dict.get('password')
-    dbname = secret_dict.get('dbname')
-
     # secrets with endpoint 
     response = client.get_secret_value(SecretId=secrets_list[1])
     secret = response['SecretString']
@@ -24,39 +16,35 @@ def get_db_credentials():
     #endpoint = secret_dict.get('connection_string')
     endpoint= "postgresql://db_admin:admin_password@terraform-20250202092136305400000004.cneu60kskjor.sa-east-1.rds.amazonaws.com:5432/db_postgres"
 
-    print(f"db_username {username}")
-    print(f"db_password {password}")
-    print(f"dbname {dbname}")
-    print(f"endpoint {endpoint}")
 
-    connection_params = {
-        'dbname': dbname,
-        'user': username,
-        'password': password,
-        'host': endpoint,
-        'port': 5432
-    }
     try:
         conn = psycopg2.connect(endpoint)
         print("Połączono z bazą danych")
     except Exception as e:
         print(f"Nie udało się połączyć z bazą danych: {e}")
-    
+    cursor = conn.cursor()
     cursor.execute("""
     CREATE TABLE data_raw (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);""")
-
-
     cursor.execute("""
     CREATE TABLE data_proceed (
     id SERIAL PRIMARY KEY,
     raw_id INTEGER REFERENCES data_raw(id),
     processed_content TEXT,
     processed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);""")
+    conn.commit()
 
-    cursor.execute("SELECT * FROM data_proceed")
+    # Zamknięcie kursora i połączenia
+    cursor.execute("SELECT * FROM data_raw")
+
+        # Pobieranie wszystkich wierszy
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
     
 if __name__ == "__main__":
     get_db_credentials()
