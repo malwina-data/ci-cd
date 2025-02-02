903aee908d95a53e7437c351527f2e834c481fce
Malwinka <malwina.fila@gmail.com>
2025-02-02 15:46:38 +0100

    w

--- code/dostep_secrets.py
+++ code/dostep_secrets.py
@@ -1,18 +1,6 @@
 import boto3
 
-def list_secrets(region="sa-east-1"):
-    client = boto3.client('secretsmanager', region_name=region)
-    response = client.list_secrets()
-    for secret in response.get('SecretList', []):
-        print(f"Name: {secret.get('Name')}, ARN: {secret.get('ARN')}")
-
-if __name__ == "__main__":
-    list_secrets()
-import boto3
-import json
-
-def get_db_credentials(secret_name):
-    # Utwórz klienta Secrets Manager
+def get_db_credentials():
     client = boto3.client('secretsmanager', region_name='sa-east-1')
     response = client.list_secrets()
     secrets_list = []
@@ -35,3 +23,6 @@ def get_db_credentials(secret_name):
     print(f"db_username {db_username}")
     print(f"db_password {db_password}")
     print(f"db_username {endpoint}")
+    
+if __name__ == "__main__":
+    get_db_credentials()
